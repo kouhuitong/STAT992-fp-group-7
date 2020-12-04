47949e21e1aac7d22b0fd1801aa524900b8cfb66
Kathryn Schmidt <40326991+kathschmidt@users.noreply.github.com>
2020-12-04 10:27:16 -0600

    Updated readme with task 6

--- scripts/6_tree_distances.sh
+++ scripts/6_tree_distances.sh
@@ -1,16 +1,21 @@
-# This shell script was written for Mac OS operating system
+# This shell script was written for Mac OS operating system.
+# Run this script in the main directory.
 # This script uses IQ-TREE to calculate the Robinson-Foulds (RF) distance between pairs of (unrooted) trees from 30 blocks (435 pairs of trees) and between pairs of trees from consective blocks only (29 pairs).
 
-# concatenated all .treefile files into one and move to treedist/
-touch treedist/chr1-all.tre
-for file in $(ls iqtree/chr*.treefile)
+# Extract chromosome number from .treefile
+chrom=$(ls -d $(ls iqtree/*.treefile | head -1) | xargs basename | cut -d _ -f1)
+
+# Concatonate all .treefile files into one and move to treedist/
+touch treedist/$chrom-all.tre
+
+for file in $(ls iqtree/$chrom*.treefile)
 do
-cat $file >> treedist/chr1-all.tre
+cat $file >> treedist/$chrom-all.tre
 done
 
 cd treedist/
 # comparing distance between all trees
-iqtree -t chr1-all.tre -rf_all -T AUTO --no-log -quiet
+iqtree -t $chrom-all.tre -rf_all -T AUTO --no-log -quiet
 
 # compare pairs of adjacent trees (i.e. trees on adjacent lines in the file)
-iqtree -t chr1-all.tre -rf_adj -T AUTO -quiet -pre chr1-all.adj.tre
+iqtree -t $chrom-all.tre -rf_adj -T AUTO -quiet -pre $chrom-all.adj.tre
