9de6c1e5343f4b9006c0e0af9ebc3baced85755f
kouhuitong <kouhuitong@126.com>
2020-12-06 00:03:01 +0800

    start work on task7

---
+++ scripts/7_plots.R
@@ -0,0 +1,11 @@
+
+dist=t(read.table('../treedist/chr1-all.adj.tre.rfdist',sep = " ",skip = 1))
+d=density(dist,na.rm = T)
+d$y[d$x < 0] <- 0
+plot(d)
+
+n=216
+f=function(z,n=216){
+  result=(1/8)^(n-3-z/2)/factorial(n-3-z/2)*exp(-1/8)
+  return(result)
+  }
