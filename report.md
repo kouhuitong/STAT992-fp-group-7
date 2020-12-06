a417ce92988089ddaa816d2d2685c26513eed1a8
kouhuitong <kouhuitong@126.com>
2020-12-06 20:51:01 +0800

    modify report.md and update r code and upload the images

--- report.md
+++ report.md
@@ -27,3 +27,13 @@ based on extrapolating from your 30-block analysis.
 - Are the observed tree distances smaller than expected if  the 2 trees were chosen at random uniformly?  We would think so, if each plant was from a distinct population  and if populations did not mix.
 
 - Do trees from consecutive blocks tend to be more similar to each other (at smaller distance) than trees from randomly chosen blocks   from the same chromosome?  We would expect so if blocks were small, due to less "recombination"  between neighboring blocks than between blocks at opposing ends of the chromosome.
+
+  ![7a](images\7a.png)
+
+  The observed tree distances are smaller than expected if the 2 trees were chosen at random uniformly.
+
+  
+
+  ![7b](images\7b.png)
+
+This plot show that the distance of Randomly Chosen Blocks is larger than that of  Consecutive Blocks.
--- scripts/7_test_for_tree_similarity.R
+++ scripts/7_test_for_tree_similarity.R
@@ -4,7 +4,7 @@ dist=read.table('../treedist/chr1-all.tre.rfdist',sep = " ",skip = 1)
 #remove na
 na_len=sum(is.na(dist[1,]))
 len=length(dist[1,])
-dist=dist[,c((len-na_len+2):len)]
+dist=dist[,c((na_len+2):len)]
 
 #get the distance vector of each 2 different trees
 d=c()
@@ -16,50 +16,64 @@ for (i in 1:length(dist[1,])) {
   }
 }
 
-
-n=length(dist[,1])
-#n=5
+#n is the number of strains
+n=216
 #calculate S=n-3-D/2
 s=n-3-d/2
-h=hist(s,freq = F,breaks = c(0:max(s+1)),col = 'blue')
+rp=rpois(length(s),1/8)
+png(filename="../images/7a.png")
+h=hist(s,freq = F,breaks = c(0:max(s+1)),col = rgb(0,0,1,0.5),ylim = c(0,1),
+       main='Distance of Randomly Chosen Blocks and Poison Distribution')
+hist(rp,freq = F,add=T,breaks = c(0:max(s+1)),ylim = c(0,1),col=rgb(1,0,0,0.5))
 
-
-#D=c(f(0,n),f(1,n),f(2,n),f(3,n))
-S=dpois(x=0:max(s),lambda=1/8)
-
-png(filename="7a.png")
-#blue stands for 6a
-barplot(h$density,col = rgb(0,0,1,0.5),main='Compare Results of 6(a) and the Poisson Dist of S',names.arg=c(0:max(s)),ylim =c(0,1) )
-#red stands for the poisson dist
-barplot(S,add = T,col=rgb(1,0,0,0.5))
+# S=dpois(x=c(0:max(s)),lambda=1/8)
+# barplot(S,add = T,col=rgb(1,0,0,0.5))
+abline(v=mean(s),col = rgb(0,0,1,0.5),lty=2,lwd=2)
+abline(v=mean(rp),col=rgb(1,0,0,0.5),lty=2,lwd=2)
 legend("topright", 
-       legend = c("6(a)", "Poisson of S"), 
+       legend = c("Randomly Chosen", "Poisson Distribution"), 
        fill = c(rgb(0,0,1,0.5),rgb(1,0,0,0.5)))
 dev.off()
+# png(filename="../images/7a.png")
+# #blue stands for 6a
+# barplot(h$density,col = rgb(0,0,1,0.5),main='Distance of Randomly Chosen Blocks and Poison Distribution',names.arg=c(min(s):max(s+1)),ylim =c(0,0.5))
+# #red stands for the poisson dist
+# barplot(S,add = T,col=rgb(1,0,0,0.5))
+# legend("topright", 
+#        legend = c("Randomly Chosen", "Poisson Distribution"), 
+#        fill = c(rgb(0,0,1,0.5),rgb(1,0,0,0.5)))
+# dev.off()
 
 #------------------------------------------------------#
 #7(b)
 #result in 6(b)
 dist2=t(read.table('../treedist/chr1-all.adj.tre.rfdist',sep = " ",skip = 1))
 #remove na
-dist2=dist2[!is.na(dist2)]
+dist2=dist2[!is.na(dist2)& dist2!=0]
 d2=dist2
 s2=n-3-d2/2
-h2=hist(s2,freq = F,breaks = c(0:max(s2)),col = 'blue')
+#h2=hist(s2,freq = F,breaks = c(0:max(s2)),col = 'blue')
 
-png(filename="7b.png")
-#blue stands for 6a
-barplot(h$density,col = rgb(0,0,1,0.5),main='Compare Results of 6(a) and 6(b)',names.arg=c(0:max(s)),ylim=c(0,1))
-#green stands for 6b
-barplot(h2$density,add = T,col=rgb(0,1,0,0.5))
+# png(filename="../images/7b.png")
+# #blue stands for 6a
+# barplot(h$density,col = rgb(0,0,1,0.5),main='Compare Results of 6(a) and 6(b)',names.arg=c(0:max(s)),ylim=c(0,1))
+# #green stands for 6b
+# barplot(h2$density,add = T,col=rgb(0,1,0,0.5))
+# legend("topright", 
+#        legend = c("6(a)", "6(b)"), 
+#        fill = c(rgb(0,0,1,0.5),rgb(0,1,0,0.5)))
+# dev.off()
+png(filename="../images/7b.png")
+h=hist(s,freq = F,breaks = c(0:max(max(s),max(s2))),col = rgb(0,0,1,0.5),ylim = c(0,0.4),main='Distance of Randomly Chosen Blocks and Consecutive Blocks')
+h=hist(s2,freq = F,breaks = c(0:max(max(s),max(s2))),col=rgb(0,1,0,0.5),add=T)
+#add vertival mean line
+abline(v=mean(s),col = rgb(0,0,1,0.5),lty=2,lwd=2)
+abline(v=mean(s2),col=rgb(0,1,0,0.5),lty=2,lwd=2)
+
 legend("topright", 
-       legend = c("6(a)", "6(b)"), 
+       legend = c("Randomly Chosen", "Consecutive"), 
        fill = c(rgb(0,0,1,0.5),rgb(0,1,0,0.5)))
 dev.off()
-
-
-
-
 
 
 
