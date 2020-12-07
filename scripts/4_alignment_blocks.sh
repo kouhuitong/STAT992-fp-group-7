3faa0800ad6913f4520d66bc1eebbec7473551a6
Haohao <haroldsu.us@gmail.com>
2020-12-07 11:37:57 +0800

    Add an optional input block_length in script 4

--- scripts/4_alignment_blocks.sh
+++ scripts/4_alignment_blocks.sh
@@ -1,6 +1,7 @@
 # This shell script was written for Mac OS operating system
 # This script uses the output of `3_build_individual_genomes.sh` to generate a given number of blocks of nucleotides of with a set length
-# The script takes three inputs chrom (1-5, C or M) starting position (e.g 1) and number of blocks to produce (e.g. 1,2,..)
+# The script takes four inputs chrom (1-5, C or M) starting position (e.g 1), number of blocks to produce (e.g. 1,2,..) 
+# and the block size (optional; 100000 by default).
 
 #set the chromosome
 chrom=$1
@@ -8,6 +9,13 @@ chrom=$1
 start=$2
 #set number of blocks to produce
 block_num=$3
+#set the block size, optionally (100000 by default)
+if[[ $# -eq 4 ]]
+then
+    block_length=$4
+else
+    block_length=100000
+fi
 
 # Here the set block length is 10000
 length_chrom=$(tail -n +1 data/TAIR10_chr$chrom.fas | wc -c) # extract length of chromosome 
@@ -16,16 +24,17 @@ for block in $(seq 1 $block_num); do # loop based on number of blocks
     if [[ $block -eq 1 ]]
     then 
         block_begin=$start
-        block_end=$(expr "$start" + 9999)
+        block_end=$(expr "$start" + "$block_length" - 1)
     else
         block_begin=$(expr "$block_end" + 1)
-        block_end=$(expr "$block_begin" + 9999)
+        block_end=$(expr "$block_begin" + "$block_length" - 1)
     fi
     if [[ $block_end -gt $length_chrom ]]
     then 
         $block_end=$length_chrom # accounts for end block that may be shorter than rest
     fi
     echo "$block_begin - $block_end bp for block $block"
+    # Call the script for task 3
     scripts/3_build_individual_genomes.sh $chrom $block_begin $block_end
     # Change filename
     mv alignments/chr${chorm}_${block_begin}_to_${block_end}.phy alignments/chr${chorm}_${block_begin}.phy
